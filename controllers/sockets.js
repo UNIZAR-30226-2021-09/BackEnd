00bd74c05e7135988841f64d95fc7de9da95dc66
AlonsoDRDLV <71894461+AlonsoDRDLV@users.noreply.github.com>
2021-05-25 07:28:16 +0200

    Update sockets.js

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -29,7 +29,10 @@ io.on("connection", (socket) => {
     let friendSocket = userSockets.get(user.nombreUsuario.toString());
     console.log("llega peticion de amistad" + user.nombreUsuario.toString());
     console.log(friendSocket);
-    
+    if(typeof friendSocket === 'undefined){
+        friendSocket=""
+       }
+    console.log(friendSocket);
     mustSend=true;
     io.sockets.connected[friendSocket].emit("llegaInvitacion", () => 
       mustSend = false
